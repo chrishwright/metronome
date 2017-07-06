fd974756d9ada60e703cbf0b902c98d12a7e2b23
Christopher Wright <christopherwright@Christophers-MacBook-Pro-2.local>
2017-07-06 07:52:11 -0400

    redesign 1

--- public/js/metronomeRoute.js
+++ public/js/metronomeRoute.js
@@ -1,15 +1,15 @@
 metronomeApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
-        template: ''
-    })
-    .when('/artist_tempo_selection', {
-        templateUrl: 'songs_by_artist_form.html'
-    })
-    .when('/song_tempo_selection', {
-    	templateUrl: 'songs_by_title_form.html'
-    })
-    .when('/listings', {    
-    	templateUrl: 'listings.html'
-    })
-	.otherwise({redirectTo: '/'});
+            template: ''
+        })
+        .when('/artist_tempo_selection', {
+            templateUrl: 'songs_by_artist_form.html'
+        })
+        .when('/song_tempo_selection', {
+            templateUrl: 'songs_by_title_form.html'
+        })
+        .when('/listings', {
+            templateUrl: 'listings.html'
+        })
+        .otherwise({ redirectTo: '/' });
 }]);
